e263bcae745e255caaa6a7771cbd0a9939f9177b
Maksym Derbenev <dereban25@gmail.com>
2019-08-28 15:32:45 +0300

    Add key ssh for aws

--- Lesson5/WebServer.tf
+++ Lesson5/WebServer.tf
@@ -1,6 +1,6 @@
 //----Web_Server----//
 provider "aws" {
-  region = "us-west-1" //N.California
+  region = "us-west-1"
 }
 
 resource "aws_eip" "elastic_ip" {
@@ -9,29 +9,29 @@ resource "aws_eip" "elastic_ip" {
 resource "aws_instance" "Web_Server" {
   ami                    = "ami-00fc224d9834053d6" #Amazon linux AMI
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Security_Web.id]
-  key_name = "N.California"
-  tags = {
-    Name = "Terraform_test"
+  vpc_security_group_ids = [ aws_security_group.Security_Web.id ]
+  key_name               = "N.California"
+  tags                   = {
+	Name = "Terraform_test"
   }
 }
 resource "aws_security_group" "Security_Web" {
-  name        = "Security group for Web_2"
+  name        = "Security group"
   description = "Test security of Terraform"
   dynamic "ingress" {
-    for_each = ["80", "443", "8080"]
+	for_each = [ "80", "443", "8080" ]
 
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+	content {
+	  from_port   = ingress.value
+	  to_port     = ingress.value
+	  protocol    = "tcp"
+	  cidr_blocks = [ "0.0.0.0/0" ]
+	}
   }
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+	from_port   = 0
+	to_port     = 0
+	protocol    = "-1"
+	cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
